ed30c1814faafc67396cb4614f3188131391852a
Ranjit67 <sahooranjit519@gmail.com>
2021-01-08 14:40:35 +0530

    api middelware

--- app.js
+++ app.js
@@ -38,13 +38,8 @@ app.use(async (req, res, next)=> {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
-  res.header('Access-Control-Allow-Headers', 'content-type');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.header('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
   next();
 });
 
