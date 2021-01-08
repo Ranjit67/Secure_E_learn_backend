d08e2b2b461d479011bcf819c310dcc81a50ba08
Ranjit67 <sahooranjit519@gmail.com>
2021-01-08 14:48:07 +0530

    api middelware error1

--- app.js
+++ app.js
@@ -32,13 +32,13 @@ app.use(express.json());
 app.use(async (req, res, next)=> {
 
   // Website you wish to allow to connect
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'ontent-Type');
 
   next();
 });
