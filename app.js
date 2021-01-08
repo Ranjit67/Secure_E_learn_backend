914b8659a357c01c073f79e97ea5315d60837360
Ranjit67 <sahooranjit519@gmail.com>
2021-01-08 14:32:24 +0530

    header fix

--- app.js
+++ app.js
@@ -28,6 +28,26 @@ mongoose.connection.on("error",(err)=>{
 
 app.use(express.json());
 
+//access api middelware 
+app.use(async (req, res, next)=> {
+
+  // Website you wish to allow to connect
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.header('Access-Control-Allow-Headers', 'content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.header('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use(require("./routes/auth"));
 
 
