7692a40500797adbd2f2e5be76aad2aa2ef3d0d6
Ranjit67 <sahooranjit519@gmail.com>
2021-01-08 21:14:25 +0530

    error midelware 7

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ app.use(morgan("dev"))
 // app.use(cors())
 
 //access api middelware 
-let allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Headers', "*");
-  next();
-}
-app.use(allowCrossDomain);
+// let allowCrossDomain = function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', "*");
+//   res.header('Access-Control-Allow-Headers', "*");
+//   next();
+// }
+// app.use(allowCrossDomain);
 
 app.get("/", (req,res,next)=>{
   res.send("modan marunu tu.");
