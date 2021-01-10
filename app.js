e45de3362499c2286afbea769cb33221b6826ae4
Ranjit67 <sahooranjit519@gmail.com>
2021-01-10 13:35:00 +0530

    test 10

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express= require("express");
+const bodyParser = require('body-parser')
 require('dotenv').config();
 const mongoose= require("mongoose");
 const morgan = require("morgan");
@@ -16,6 +17,8 @@ const app = express();
 //    changeOrigin: true }));
 
 app.use(morgan("dev"))
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 // var corsOptions = {
 //   origin: 'http://localhost:9000',
@@ -32,7 +35,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
 
   // Request headers you wish to allow
   res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
@@ -40,6 +43,10 @@ app.use(function (req, res, next) {
 // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
   // Pass to next layer of middleware
+  if(req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    return res.status(200).json({})
+  }
   next();
 });
 
